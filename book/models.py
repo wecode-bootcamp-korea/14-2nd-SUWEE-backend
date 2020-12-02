bc0dfd4d5cea30b6489d9c8c7ac86a65b2350370
Hyun Suk <cs982606@gmail.com>
2020-12-02 16:59:38 +0900

    model 수정중

--- book/models.py
+++ book/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=45)
+    subtitle = models.CharField(max_length=45)
+    image_url = models.CharField(max_length=200)
+    company = models.CharField(max_length=45)
+    author = models.CharField(max_length=45)
+    about_author = models.TextField()
+    contents = models.TextField()
+    company_review = models.TextField()
+    page = models.IntegerField()
+    publication_date = models.DateField()
+    description = models.TextField()
+    category =
+    keyword =
+
+    class Meta:
+        db_table = 'books'
+
+class Review(models.Model):
+    
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'categories'
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'keywords'
+
+class Today(models.Model):
+    description = models.TextField()
+    pick_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'today'
+
+class Like(models.Model):
+    review = models.ForeignKey('Review', on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'likes'
+
+
---
+++ db_uploader.py
@@ -0,0 +1,86 @@
+import os
+import django
+import csv
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suwee.settings")
+django.setup()
+
+from user.models import User, UserBook
+from book.models import 
+from library.models import 
+from payment.models import 
+
+csv_path = './csv_data/users.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        User.objects.create(nickname=row[0], phone_number=row[1], password=row[2], email=row[3], lmage_url=row[4])
+
+csv_path = './csv_data/users_books.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        UserBook.objects.create(user_id=row[0], book_id=row[1], progress=row[2], time=row[3])
+
+csv_path = './csv_data/reviews.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        Review.objects.create(user_id=row[0], book_id=row[1], contents=row[2])
+
+csv_path = './csv_data/likes.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        Like.objects.create(review_id=row[0], user_id=row[1])
+
+
+csv_path = './csv_data/libraries_books.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        LibraryBook_objects.create(library_id=row[0], book_id=row[1])
+
+
+csv_path = './csv_data/libraries.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        Library.objects.create(user_id=row[0], name=row[1])
+
+
+csv_path = './csv_data/categories.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        Category.objects.create(name=row[0])
+
+
+csv_path = './csv_data/books.csv'
+with open(csv_path) as csv_file:
+    row = csv.reader(csv_file, delimiter=',')
+    next(rows)
+    for row in rows:
+        Book.objects.create(
+            title=row[0],
+            subtitle=row[1],
+            image_url=row[2],
+            company=row[3],
+            author=row[4],
+            about_author=row[5],
+            company_review=row[6],
+            category_id=row[7],
+            page=row[8],
+            publication_date=row[9],
+            description=row[10]
+        )
+
+print ('데이터베이스에 데이터 추가')
