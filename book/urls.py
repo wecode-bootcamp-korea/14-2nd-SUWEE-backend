b6d2de87f7ba29af89cf61421ded628f69ee6e77
Hyun Suk <cs982606@gmail.com>
2020-12-05 00:32:19 +0900

    Add : detailview 수정중

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views  import BookDetailView
-
+from .views      import RecentlyBookView, BookDetailView
 
 urlpatterns = [
+    path('/recently', RecentlyBookView.as_view()),
     path('/<int:book_id>', BookDetailView.as_view()),
     path('/save/<int:book_id>', BookDetailView.as_view()),
 
